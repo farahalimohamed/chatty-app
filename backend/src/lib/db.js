38073cd94749c69b0f9ac2a10be251107deef43c
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-10-28 22:47:58 +0300

    first commit

---
+++ backend/src/lib/db.js
@@ -0,0 +1,10 @@
+import monogoose from 'mongoose';
+
+export const connectDB = async () => {
+    try{
+        const conn = await monogoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch(error){
+        console.log("MongoDB  connection error:", error);
+    }
+};
