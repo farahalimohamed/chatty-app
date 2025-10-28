0d16a61b9a7bea1a23119318796f2033a11a3fad
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-10-29 02:21:34 +0300

    create axois instance and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import SignUpPage from "./components/pages/SignUpPage";
+import LoginPage from "./components/pages/LoginPage";
+import SettingsPage from "./components/pages/SettingsPage";
+import ProfilePage from "./components/pages/ProfilePage";
+
 const App = () => {
   return (
-    <div className="text-red-400">
-      <button className="btn">Default</button>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
