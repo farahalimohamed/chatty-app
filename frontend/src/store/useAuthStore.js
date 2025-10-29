4759033206f105ba7c74f716c57660d8661262c3
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-10-29 04:52:27 +0300

    handle logout method

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -32,5 +32,15 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({isSigningUp: false});
         }
+    },
+
+    logout:async() =>{
+        try {
+            const res = await axiosInstance.post('/auth/logout');
+            set({authUser: null});
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error.response?.data?.message);
+        }
     }
 }));
