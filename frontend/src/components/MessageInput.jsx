ec508d6299cb9095e8f6c2cb300040c054e4616f
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-10-30 02:39:45 +0300

    handle sending messages functionality

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
 import { Image, Send, X } from "lucide-react";
+import toast from "react-hot-toast";
 
 const MessageInput = () => {
     const [text, setText] = useState("");
@@ -9,12 +10,43 @@ const MessageInput = () => {
     const {sendMessage} = useChatStore();
 
     const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if(!file.type.startsWith('image/')) {
+            toast.error("Please select a valid image file");
+            return;
+        }
 
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImagePreview(reader.result);
+        };
+        reader.readAsDataURL(file);
     };
 
-    const removeImage = () => {};
+    const removeImage = () => {
+        setImagePreview(null);
+        if(fileInputRef.current) {
+            fileInputRef.current.value = "";
+        }
+    };
 
-    const handleSendMessage = () => {};
+    const handleSendMessage = async (e) => {
+        e.preventDefault();
+        if(!text.trim() && !imagePreview) return;
+        try {
+            await sendMessage({text:text.trim(), image:imagePreview});
+
+            // clear form
+            setText("");
+            setImagePreview(null);
+            if(fileInputRef.current) {
+                fileInputRef.current.value = "";
+            }
+        } catch (error) {
+            toast.error("Failed to send message", error);
+        }
+    };
     return (
       <div className="p-4 w-full">
         {imagePreview && (
