af826e38d16c5811c1427f4d4b8418a40c89df37
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-10-30 01:09:49 +0300

    handle chat container ui

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore"
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
+    const {selectedUser, messages, getMessages, isMessagesLoading} = useChatStore();
+
+    useEffect(() => {
+        getMessages(selectedUser._id);
+    }, [getMessages, selectedUser._id])
+
+    if(isMessagesLoading) {
+        return (
+            <div className="flex-1 flex flex-col overflow-auto">
+                <ChatHeader />
+                <MessageSkeleton />
+                <MessageInput />
+            </div>
+            );
+    }
+
+
+    return (
+        <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+
+        <p>messages.....</p>
+
+        <MessageInput />
+        </div>
+    );
 }
 
 export default ChatContainer
