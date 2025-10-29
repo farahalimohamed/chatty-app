0027321aef5f24e5c4e21d61ba8b9683a4919c39
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-10-30 01:08:28 +0300

    handle send message

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -33,6 +33,16 @@ export const useChatStore = create((set, get) => ({
         }
     },
 
+    sendMessage: async (messageData) => {
+        const {selectedUser, messages} = get();
+        try {
+            const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+            set({messages:[...messages, res.data]});
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+
+    },
     // todo: optimize this one later 
     setSelectedUser: (selectedUser) => {
         set({ selectedUser });
