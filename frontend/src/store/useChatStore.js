6408e8ce13b7f3895c6f490d765d8a159a246a6b
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-10-30 05:12:06 +0300

    create user seed

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -50,8 +50,10 @@ export const useChatStore = create((set, get) => ({
         if(!selectedUser) return;
 
         const socket = useAuthStore.getState().socket;
-        // todo: optimize this one later 
+        
         socket.on("newMessage", (newMessage) => {
+            const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+            if (!isMessageSentFromSelectedUser) return;
             set({messages: [...get().messages, newMessage]});
         });
     },
@@ -61,7 +63,6 @@ export const useChatStore = create((set, get) => ({
         socket.off("newMessage");
     },
 
-    // todo: optimize this one later 
     setSelectedUser: (selectedUser) => {
         set({ selectedUser });
     },
